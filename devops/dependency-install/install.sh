1a382dc69f925f861dfaa3d984c54c69d321d3e5
subashglobalyhub <subash.chaudhary@globalyhub.com>
2024-06-21 11:17:00 +0545

    inital-setup:2

--- devops/dependency-install/install.sh
+++ devops/dependency-install/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# ------- LARGE Banner display section start
+
 function print_separator {
     printf "\n%s\n" "--------------------------------------------------------------------------------"
 }
@@ -9,107 +13,117 @@ function print_header {
     print_separator
 }
 
-# Function to check if Figlet is installed
+# Detection in Yellow color
+function print_init {
+    local message="$1"
+    printf "\e[33m%s\e[0m\n" "$message"  
+}
+
+# Intermediate in Blue color
+function print_intermediate {
+    local message="$1"
+    printf "\e[34m%s\e[0m\n" "$message"  
+}
+
+# Completion in Green color
+function print_success {
+    local message="$1"
+    printf "\e[1m\e[32m%s\e[0m\n" "$message"  
+    print_separator
+}
+
+# Failures in Red color
+function print_fail {
+    local message="$1"
+    printf "\e[1m\e[31m%s\e[0m\n" "$message"  
+    print_separator
+}
+
 check_figlet_installed() {
     print_separator
     print_header "1 - FIGLET"
     print_separator
     sleep 2
     if command -v figlet &>/dev/null; then
-        echo -e "\e[1m\e[32mFiglet is already installed\e[0m"  # Print in bold and green color
+        print_success "Figlet is already installed"  
         return 0
     else
+        print_fail "Figlet package is not found in system"
         return 1
     fi
 }
 
-# Function to check if Docker is installed
 check_docker_installed() {
     print_separator
     print_header "2 - DOCKER"
     print_separator
     sleep 2
+
+    docker_installed=true
+    docker_compose_installed=true
+
     if command -v docker &>/dev/null; then
-        echo -e "\e[1m\e[32mDocker is already installed\e[0m"  # Print in bold and green color
+        print_success "Docker is already installed"
+    else
+        print_fail "Docker Package is missing"
+        docker_installed=false
+    fi
+
+    if docker compose version &>/dev/null; then
+        print_success "Docker Compose is already installed"
+    else
+        print_fail "Docker Compose Package is missing"
+        docker_compose_installed=false
+    fi
+
+    if [ "$docker_installed" = true ] && [ "$docker_compose_installed" = true ]; then
         return 0
     else
-        echo -e "\e[1m\e[31mDocker is not installed\e[0m"  # Print in bold and red color
         return 1
     fi
 }
 
-
-# Function to check if Trivy is installed
 check_trivy_installed() {
     print_separator
-    print_header "4 - TRIVY"
+    print_header "3 - TRIVY"
     print_separator
     sleep 2
     if command -v trivy &>/dev/null; then
-        echo -e "\e[1m\e[32mTrivy is already installed\e[0m"  # Print in bold and green color
+        print_success "Trivy is already installed"  
         return 0
     else
-        echo -e "\e[1m\e[31mTrivy is not installed\e[0m"  # Print in bold and red color
+        echo -e "Trivy is not found in system" 
         return 1
     fi
 }
 
-# Function to install Figlet
+
 install_figlet() {
     if check_figlet_installed; then
         return
     fi
-
-    if grep -q 'Amazon Linux 2' /etc/os-release || grep -q 'Amazon Linux 3' /etc/os-release; then
-        # Amazon Linux 2 or Amazon Linux 3
-        print_separator
-        echo "Detected Amazon Linux"
-        sudo amazon-linux-extras install epel -y
-        sudo yum -y install figlet
-    elif grep -q 'Ubuntu' /etc/os-release; then
-        # Ubuntu
-        print_separator
-        echo "Detected Ubuntu"
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
+    if grep -q 'Ubuntu' /etc/os-release; then
+        print_init "Initializing Figlet installation process"
         sudo apt-get update
         sudo apt-get install -y figlet
-    elif grep -qEi 'redhat|centos' /etc/os-release; then
-        # Red Hat or CentOS
-        print_separator
-        echo "Detected Red Hat or CentOS"
-        sudo yum -y install figlet
     else
         echo "Unsupported Linux distribution"
         exit 1
     fi
+    print_separator
 }
 
-# Function to install Docker
 install_docker() {
     if check_docker_installed; then
         return
     fi
 
-    if grep -q 'Amazon Linux 2' /etc/os-release; then
-        # Amazon Linux 2
-        echo "Detected Amazon Linux 2"
-        sudo yum update -y
-        yes | sudo amazon-linux-extras install docker
-        echo -e "\e[1m\e[32mDocker installed successfully\e[0m"  # Print in bold and green color
-        sudo systemctl start docker
-        sudo systemctl enable docker
-        echo "Docker service started and enabled"
-    elif grep -q 'Amazon Linux 3' /etc/os-release; then
-        # Amazon Linux 3
-        echo "Detected Amazon Linux 3"
-        sudo yum update -y
-        yes | sudo yum install -y docker
-        echo -e "\e[1m\e[32mDocker installed successfully\e[0m"  # Print in bold and green color
-        sudo systemctl start docker
-        sudo systemctl enable docker
-        echo "Docker service started and enabled"
-
-    elif grep -q 'Ubuntu' /etc/os-release; then
-        # Ubuntu
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
+    if grep -q 'Ubuntu' /etc/os-release; then
+        print_init "Initializing Docker installation process"
         echo "Detected Ubuntu"
         sudo apt-get update
         sudo apt-get install -y ca-certificates curl
@@ -122,10 +136,13 @@ install_docker() {
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
         yes | sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-        echo -e "\e[1m\e[32mDocker installed successfully\e[0m"  # Print in bold and green color
+        print_success "Docker installed successfully"
+        print_init "starting and enabling Docker service"
         sudo systemctl start docker
         sudo systemctl enable docker
-        echo "Docker service started and enabled"
+        print_success "Docker service started and enabled"
+    fi
+    print_separator
 }
 
 install_trivy() {
@@ -133,9 +150,10 @@ install_trivy() {
         return
     fi
 
+    os_description=$(lsb_release -a 2>/dev/null | grep "Description:" | awk -F'\t' '{print $2}')
+    print_init "$os_description Detected on your system"
     if grep -q 'Ubuntu' /etc/os-release; then
-        # Ubuntu
-        echo "Detected Ubuntu"
+        print_init "Initializing Docker installation process"
         print_separator
         echo "Installing Trivy for Ubuntu"
         sudo apt-get update
@@ -144,18 +162,16 @@ install_trivy() {
         echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
         sudo apt-get update
         sudo apt-get install -y trivy
-        echo -e "\e[1m\e[32mTrivy installed successfully\e[0m"  # Print in bold and green color
+        print_success "Trivy installed successfully"
         trivy image --download-db-only
+    else
+        echo "Unsupported Linux distribution"
+        exit 1
+    fi
+    print_separator
 }
 
-
-# Function to print separator
-print_separator() {
-    echo "----------------------------------------"
-}
-
-main ()
-{
+main () {
 install_figlet
 install_docker
 install_trivy
